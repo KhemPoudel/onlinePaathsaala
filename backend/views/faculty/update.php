448b7286869387e2ca9240660cd05076f080c606
John Sunam <sunamjohn@gmail.com>
2016-01-18 21:59:13 +0545

    update

--- backend/views/faculty/update.php
+++ backend/views/faculty/update.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 
+//use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
 /* @var $this yii\web\View */
 /* @var $model app\models\faculty\FacultyRecord */
 
@@ -12,11 +15,18 @@ $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="faculty-record-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-        'dataProvider'=>$model->id,
-    ]) ?>
+        <?php $form = ActiveForm::begin(); ?>
 
+        <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'level')->textInput(['maxlength' => true]) ?>
+
+
+
+        <div class="form-group">
+            <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
 </div>
